a2ceba77788dacc2020739c9a0e6cd4da7e5c246
Yi Xiang Chen <yxiangch@everis.com>
2020-12-15 13:39:05 +0100

    Projecto Creado

---
+++ src/main/java/com/everisapigame/EverisApiGames/EverisApiGamesApplication.java
@@ -0,0 +1,13 @@
+package com.everisapigame.EverisApiGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisApiGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisApiGamesApplication.class, args);
+	}
+
+}
