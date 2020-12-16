1840f562c9b37d93a64dabb928aa96fa488a51f8
FranciscoJoseCaroCarazo <fcarocarazo@safareyes.es>
2020-12-16 14:08:54 +0100

    Entidad y controller

--- src/main/java/com/everisapigame/EverisApiGames/Controllers/GamesController.java
+++ src/main/java/com/everisapigame/EverisApiGames/Controllers/GamesController.java
@@ -3,10 +3,14 @@ package com.everisapigame.EverisApiGames.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.everisapigame.EverisApiGames.Entities.Games;
 import com.everisapigame.EverisApiGames.Services.GamesServicesI;
 
 @RestController
@@ -17,11 +21,31 @@ public class GamesController {
 	@Autowired
 	private GamesServicesI gamesService;
 	
+	/**  */
 	@GetMapping
 	public ResponseEntity<?> getAllGames(){
 		return ResponseEntity.status(HttpStatus.OK).body(gamesService.getAllGames());
-		
 	}
+	
+	/**  */
+	@DeleteMapping(path = "{id}")
+	public ResponseEntity<?> deleteGamesById(@PathVariable Integer id){
+		gamesService.removeGamesByIdGames(id);
+		return ResponseEntity.status(HttpStatus.OK).body("El Juego ha sido eliminado.");
+	}
+	
+	/**  */
+	@GetMapping
+	public ResponseEntity<?> getGamesById(@PathVariable Integer id){
+		return ResponseEntity.status(HttpStatus.OK).body(gamesService.getGamesByIdGames(id));
+	}
+	
+	@PutMapping
+	public void deleteGame(@RequestBody Games game) {
+		gamesService.addGames(game);
+	}
+	
+	
 	
 	
 
